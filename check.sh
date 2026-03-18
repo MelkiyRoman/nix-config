17da863a87a9aa4fc91f03b7d32adb799a7606e5
romma <roma.melky2001@gmail.com>
2026-03-18 23:39:54 +0300

    2026-03-18 at 20:39 UTC

--- check.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko -- --mode mount home/disko.nix
-
-lsblk
-
-echo "Hello ROMMMMMMMMMMMAaAAaAAAaaA"
--- fix.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-cd /mnt/home/romma/nix-config/home
-
-nixos-generate-config --no-filesystems --show-hardware-config > modules/hardware-configuration.nix
-
-git add .
-
-nixos-install --flake .#home --impure
